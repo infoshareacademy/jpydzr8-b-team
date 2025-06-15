4b41668775ea5da495b0fb984f820fa8939a5142
Amakandu <lahmu2024@gmail.com>
2025-06-15 05:58:44 +0200

    Delete biblioteka.py

--- biblioteka.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def wczytanie_bazy_ksiazek(baza_ksiazek):
-    df = pd.read_csv('baza_ksiazek.txt', sep=';', names=['ID Książki', 'Autor', 'Tytuł', 'Ilość'])
-    df.astype(str)
-    return df
-
-plik = 'baza_ksiazek.txt'
-#print(wczytanie_bazy_książek(plik))

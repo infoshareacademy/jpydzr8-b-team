22665674f46c725cb3ea60563e5813415f22ef4a
Jacek Ciszewski <j.ciszewski@icloud.com>
2025-10-17 18:18:35 +0200

    przechwycenie user w form.save dla loggera {user.username} i {user.id} w rejestracji

--- online_library/catalog/urls.py
+++ online_library/catalog/urls.py
@@ -11,5 +11,4 @@ urlpatterns = [
     path('my-books/return/<int:book_id>/', views.return_book, name='return_book'),
     path('reports/borrowed-users/', views.borrowed_users_report, name='borrowed_users_report'),
     path('logs/', views.view_logs, name='view_logs'),
-
 ]
